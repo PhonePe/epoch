000e96c7bc70edf6b1f693a863cb7720477e14cf
Santanu Sinha <santanu.sinha@gmail.com>
2023-03-17 12:25:07 +0530

    Made blackhole type serializable

--- epoch-models/src/main/java/com/phonepe/epoch/models/notification/NotificationSpec.java
+++ epoch-models/src/main/java/com/phonepe/epoch/models/notification/NotificationSpec.java
@@ -10,7 +10,8 @@ import lombok.Data;
 @Data
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "type")
 @JsonSubTypes({
-        @JsonSubTypes.Type(name = "MAIL", value = MailNotificationSpec.class)
+        @JsonSubTypes.Type(name = "MAIL", value = MailNotificationSpec.class),
+        @JsonSubTypes.Type(name = "BLACKHOLE", value = BlackholeNotificationSpec.class),
 })
 public abstract class NotificationSpec {
     private final NotificationReceiverType type;
