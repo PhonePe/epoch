3a01df410bedcc168f6ffd8086d86d479371cd0b
Santanu Sinha <santanu.sinha@gmail.com>
2024-03-29 09:32:32 +0530

    Null or empty check in script

--- epoch-server/entrypoint.sh
+++ epoch-server/entrypoint.sh
@@ -17,10 +17,15 @@ term_handler() {
 # on callback, kill the last background process, which is `tail -f /dev/null` and execute the specified handler
 trap 'kill ${!}; term_handler' SIGTERM
 
-export EPOCH_ADMIN_PASSWORD="${ADMIN_PASSWORD-admin}"
-export EPOCH_GUEST_PASSWORD="${GUEST_PASSWORD-guest}"
-export DROVE_USERNAME="${DROVE_USERNAME-admin}"
-export DROVE_PASSWORD="${DROVE_PASSWORD-password}"
+if [ -z "${DROVE_ENDPOINT}" ]; then
+    echo "Error: DROVE_ENDPOINT is a mandatory parameter for epoch to work."
+    exit 1
+fi
+
+export EPOCH_ADMIN_PASSWORD="${ADMIN_PASSWORD:-admin}"
+export EPOCH_GUEST_PASSWORD="${GUEST_PASSWORD:-guest}"
+export DROVE_USERNAME="${DROVE_USERNAME:-admin}"
+export DROVE_PASSWORD="${DROVE_PASSWORD:-password}"
 
 CONFIG_PATH=${CONFIG_FILE_PATH:-config.yml}
 
@@ -38,19 +43,19 @@ export JAVA_HOME="${JAVA_HOME}:${PWD}"
 DEBUG_ENABLED="${DEBUG-0}"
 if [ "$DEBUG_ENABLED" -ne 0 ]; then
 
-echo "Environment variables:"
-printenv
+  echo "Environment variables:"
+  printenv
 
-echo "Java version details:"
-java -version
+  echo "Java version details:"
+  java -version
 
-echo "Contents of working dir: ${PWD}"
-ls -l "${PWD}"
+  echo "Contents of working dir: ${PWD}"
+  ls -l "${PWD}"
 
 fi
 
 # run application
-CMD=$(eval echo "java -jar -XX:+${GC_ALGO-UseG1GC} -Xms${JAVA_PROCESS_MIN_HEAP-1g} -Xmx${JAVA_PROCESS_MAX_HEAP-1g} ${JAVA_OPTS} epoch-server.jar server ${CONFIG_PATH}")
+CMD=$(eval echo "java -jar -XX:+${GC_ALGO:-UseG1GC} -Xms${JAVA_PROCESS_MIN_HEAP:-1g} -Xmx${JAVA_PROCESS_MAX_HEAP:-1g} ${JAVA_OPTS} epoch-server.jar server ${CONFIG_PATH}")
 echo "Starting Epoch Server by running command: ${CMD}"
 
 eval "${CMD}" &
